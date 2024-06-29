af58aa2f704598cbf12068c191b7107c1ddf58cb
KDO <114388910+kdo2064@users.noreply.github.com>
2024-06-29 21:22:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
 import platform
-
+try:
+    import pytz
+    import httpx
+except:
+    _ = os.system('pip install illusionanime illusioncolor httpx datetime pytz' if os.name == 'nt' else 'pip3 install illusionanime illusioncolor httpx datetime pytz')
 def check_os():
     os_name = platform.system()
 
     if os_name == 'Windows':
         os.system('python .\\windows\\run.py')
     elif os_name == 'Linux':
-        if 'TERMUX_PREFIX' in os.environ:
-            print("Can't run this on Termux")
+        # Check if running in Termux
+        if 'com.termux' in os.listdir('/data/data'):
+            os.system('cd linux && python3 run.py')
         else:
             os.system('cd linux && python3 run.py')
     else:
