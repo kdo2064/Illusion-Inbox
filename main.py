b458d86bff46bd0160f4f4a97d8508d45ac6c9de
KDO <114388910+kdo2064@users.noreply.github.com>
2024-06-29 21:32:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import os
 import platform
+
 try:
     import pytz
     import httpx
-except:
-    _ = os.system('pip install illusionanime illusioncolor httpx datetime pytz' if os.name == 'nt' else 'pip3 install illusionanime illusioncolor httpx datetime pytz')
+except ImportError:
+    os.system('pip install illusionanime illusioncolor httpx datetime pytz' if os.name == 'nt' else 'pip3 install illusionanime illusioncolor httpx datetime pytz')
+
 def check_os():
     os_name = platform.system()
 
     if os_name == 'Windows':
         os.system('python .\\windows\\run.py')
     elif os_name == 'Linux':
-        if 'TERMUX_PREFIX' in os.environ:
+        whoami = os.popen('whoami').read().strip()
+        if 'TERMUX_PREFIX' in os.environ or whoami == 'u0':
             os.system('cd linux && python3 run.py')
         else:
             os.system('cd linux && python3 run.py')
